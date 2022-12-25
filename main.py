969ed0eb38e902377f8e8df2ed17ef9cec27b90a
ben <112295217+0x5b62656e5d@users.noreply.github.com>
2022-12-25 22:48:39 +0800

    Fixed comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing of libraries and definition of key variables
 import subprocess 
 import wolframalpha # pip install wolframaplha
-# wolfram alpha client key
+# WolframAlpha client key
 client = wolframalpha.Client("API keys here")
 import pyttsx3 # pip install pyttsx3
 import speech_recognition as sr # pip install speech_recognition
@@ -11,7 +11,7 @@ import webbrowser # pip install webbrowser
 from AppOpener import run
 import pyttsx3 # pip install pyttsx3
 
-# text to speech setup, engine,
+# TTS setup
 engine = pyttsx3.init('sapi5')                    
 engine.setProperty('rate', 230)     
 engine.setProperty('volume', 1.0)    
@@ -19,134 +19,133 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 currentTime = datetime.datetime.now()
 
-#@param 'audio' speaks the words put in the parameter
+# Speaks the given input
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-# greets you depending on the time of day
+# Greets user
 def greeting():
     hour = int(datetime.datetime.now().hour)
-    # if it's 12am - 12pm: good morning
+    # Returns good morning if time is 12am - 12pm
     if hour >= 0 and hour<12:
         speak("Good Morning")
-    # if it's 12pm - 18pm: good afternoon
+    # Returns good afternoon if time is 12pm - 18pm
     elif hour >= 12 and hour<18:
         speak("Good Afternoon")  
-    # if it's 18pm - 12am: good evening
+    # Returns good evening if time is 18pm - 12am
     else:
         speak("Good Evening") 
 
-#takes your command and reads it
+# Takes command
 def takeCommand():
     
     r = sr.Recognizer()
-    # sets mic as the default system one
+    # Uses default system microphone
     with sr.Microphone() as source:
          
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
-    # tries to do recognize your voice
+    # Tries to do recognize your voice
     try:
         print("Recognizing...")   
         query = r.recognize_google(audio, language = 'en-in')
         print(f"User said: {query}\n")
-    # if it doesnt recognize your voice
+    # If voice can't be recognized
     except Exception as e:
         print(e)   
         print("Unable to recognize your goofy ass voice") 
         return "None"
-    # replaces 'gerald' and 'hey' so it doesnt mess up the command
+    # Replaces 'gerald' and 'hey' so it doesnt mess up the command
     if 'Gerald' or 'hey Gerald' in query:
         query = query.replace("Gerald", "")
         query = query.replace("hey", "")
         return query
-    # if the command doesnt include 'gerald' then it doesnt register
+    # Will not register if query does not incklude "Gerald"
     else:
         query = ""
         return query 
 
-# acual script part: reads what you say and reacts accordingly
 if __name__ == '__main__':
     greeting()
     speak("what can I do for you today")
 
     while True:
         query = takeCommand()
-        # wikipedia search
+        # Wikipedia search
         if 'wikipedia' in query:  
             speak("searching wikipedia")
             query = query.replace("wikipedia", "")
             wiki_ans = wikipedia.summary(query, sentences = 2)
             speak(wiki_ans)
-        # tells you the time
+        # Returns time
         elif 'time' in query:   
             speak("The time is now: %s:%s" % (currentTime.hour, currentTime.minute))
-        # kills gerald
+        # Terminates program
         elif 'exit' in query:
             speak("killing gerald")
             speak("what a world. what a world.")
             print("you have killed Gerald, you monster")
             exit()
-        # calculates your math problem using wolfram alpha
+        # Calculates math problem via WolframAlpha
         elif 'calculate' in query:  
             query = query.replace("calculate", "")
             res = client.query(query)                             
             wolfram_res = next(res.results).text
             speak(f"the answer to {query} is {wolfram_res}")
             print(wolfram_res)
-        # searches default browser
+        # Searches in browser
         elif 'search' in query:     
             query = query.replace("search", "")
             speak("searching on the web")
             webbrowser.open(query)
-        # opens taiwan news
+        # Opens Taiwan News in browser
         elif 'news' in query:       
             speak("opening taiwan news")
             webbrowser.open("taiwannews.com.tw")
-        # shuts down windows computer
+        # Shuts down computer
         elif 'shutdown' in query:
             speak("shutting down your computer")
             subprocess.call('shutdown /s')
             exit()
-        # restarts windows computer
+        # Reboots computer
         elif "restart" in query:        
             subprocess.call(["shutdown", "/r"])
             exit()
-        # uses wolfram alpha to tell you the weather at your current location
+        # Returns weather at current location via WolframAlpha
         elif "weather" in query:
             res = client.query(query)                             
             wolfram_res = next(res.results).text
             speak(f"the weather is currently {wolfram_res}")
             print(wolfram_res)
-        # uses wolfram alpha to define your word
+        # Returns definition of word via WolframAlpha
         elif "definition" in query:
             query = query.replace("define", "")
             res = client.query(query)                             
             wolfram_res = next(res.results).text
             speak(f"the definition of {query} is {wolfram_res}")
             print(wolfram_res)
-        # uses wolfram alpha to give you synonyms for your word
+        # Returns synonyms via WolframAlpha
         elif "synonyms" in query:
             res = client.query(query)
             wolfram_res = next(res.results).text
             speak(f"synonyms for {query} are {wolfram_res}")
             print(wolfram_res)
-        # uses wolfram alpha to give you antonyms for your word
+        # Returns antonyms via WolframAlpha
         elif 'antonyms' in query:
             res = client.query(query)
             wolfram_res = next(res.results).text
             speak(f"synonyms for {query} are {wolfram_res}")
             print(wolfram_res)
-        # opens an app on your windows computer
+        # Opens an app
         elif 'open' in query:
             query = query.replace("open", "")
             speak(f"opening {query}")
             run(query)
         
-        #* Keep this at the bottomm
-        # calculates your math problem using wolfram alpha
+        #! Keep this at the bottomm
+        # Calculates math problem using WolframAlpha
         elif "what's" in query:  
             query = query.replace("what's", "")
             res = client.query(query)                             
